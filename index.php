edb3ac9b0fb048886116948307d34ee521fe8e35
Victor Feitoza <vfeitoza@gmail.com>
2013-05-08 19:08:47 -0300

    Adicionando comentario

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+// esta linha mostra o hello name
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
